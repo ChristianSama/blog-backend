b15792137d76a170f444365def7b875b7297ab93
Christian <csamaniego@stackbuilders.com>
2022-04-08 09:13:24 -0500

    add create user post endpoint

--- src/app.ts
+++ src/app.ts
@@ -5,34 +5,40 @@ const prisma = new PrismaClient();
 const app = express();
 
 //GET - /posts
-app.get('/posts', async (req, res) => {
-  const post = await prisma.post.findMany(); 
+app.get("/posts", async (req, res) => {
+  const post = await prisma.post.findMany();
   res.json(post);
-})
+});
 
 //GET - /post/:id
-app.get('/post/:id', async (req, res) => {
-  const id = req.params.id
+app.get("/post/:id", async (req, res) => {
+  const id = req.params.id;
 
-  const post = await prisma.post.findUnique( {
-    where: {id: Number(id)},
-  }); 
-
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  });
   res.json(post);
-})
+});
 
 //POST - /posts
-app.post('/posts', async (req, res) => {
-  const {title, content, authorEmail} = req.body;
-
-})
+app.post("/posts", async (req, res) => {
+  const { title, content, authorEmail } = req.body;
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    },
+  });
+  res.json(result);
+});
 
 //GET - /users
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
-})
+});
 
 const server = app.listen(3000, () => {
-  console.log('Server listening on port 3000')
-})
+  console.log("Server listening on port 3000");
+});
