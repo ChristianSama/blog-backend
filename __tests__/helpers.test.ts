6217eedd66316ad467b8ad9a9d04bd5065cd1183
Christian <csamaniego@stackbuilders.com>
2022-05-06 14:06:37 -0500

    Added more tests for helpers

--- __tests__/helpers.test.ts
+++ __tests__/helpers.test.ts
@@ -1,13 +1,45 @@
-import {countUsersABC, filterUsersABC, sortAndCapitalizeLastName} from "../src/services/helpers";
+import {capitalizeLastnames, countUsersABC, filterUsersABC, sortAndCapitalizeLastName, sortUsersByName} from "../src/services/helpers";
 import fs from 'fs'
 import path from 'path'
 
 let mocks = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./mocks/users.json")).toString())
 
+describe("sortUsersByName()", () => {
+  describe("When passed an array of users", () => {
+    it("should return an array of users sorted alphabetically by name", () => {
+      expect(sortUsersByName(mocks.users)).toEqual(mocks.usersSortedByName);
+    })
+  })
+  describe("When passed an empty array", () => {
+    it("should return an empty array", () => {
+      expect(sortUsersByName([])).toEqual([]);
+    })
+  })
+})
+
+describe("capitalizeLastNames()", () => {
+  describe("When passed an array of users", () => {
+    it("should return an array of users with their lastnames capitalized", () => {
+      expect(capitalizeLastnames(mocks.users)).toEqual(mocks.usersCap);
+    })
+    it("should return an array of users without the password property", () => {
+      expect(capitalizeLastnames(mocks.users)[0]).not.toHaveProperty("password");
+    })
+  })
+  describe("When passed an empty array", () => {
+    it("should return an empty array", () => {
+      expect(capitalizeLastnames([])).toEqual([]);
+    })
+  })
+})
+
 describe("sortAndCapitalizeLastName()", () => {
   describe("When passed an array of users", () => {
-    it("should return an array of users sorted alphabetically and with its lastnames capitalized", () => {
+    it("should return an array of users sorted alphabetically with lastnames capitalized", () => {
       expect(sortAndCapitalizeLastName(mocks.users)).toEqual(mocks.usersSortedAndCap);
+    })
+    it("should return an array of users without the password property", () => {
+      expect(sortAndCapitalizeLastName(mocks.users)[0]).not.toHaveProperty("password");
     })
   })
   describe("When passed an empty array", () => {
@@ -23,6 +55,11 @@ describe("filterUsersABC", () => {
       expect(filterUsersABC(mocks.users)).toEqual(mocks.usersABC);
     })
   })
+  describe("When passed an array without users whose name start with 'a', 'b' or 'c'", () => {
+    it("should return an empty array", () => {
+      expect(filterUsersABC(mocks.usersWithoutABC)).toEqual([]);
+    })
+  })
   describe("When passed an empty array", () => {
     it("should return an empty array", () => {
       expect(filterUsersABC([])).toEqual([]);
@@ -34,6 +71,11 @@ describe("countUsersABC", () => {
   describe("When passed an array of users", () => {
     it("should return an object with the quantities of users whose name start with 'a', 'b' and 'c' respectively", () => {
       expect(countUsersABC(mocks.users)).toEqual(mocks.countABC);
+    })
+  })
+  describe("When passed and array of users whose name don't start with 'a', 'b', 'c'", () => {
+    it("should return an object with 0 on its values", () => {
+      expect(countUsersABC(mocks.usersWithoutABC)).toEqual({a: 0, b: 0, c: 0});
     })
   })
   describe("When passed an empty array", () => {
--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -1,33 +1,40 @@
 import { User } from "@prisma/client";
 
 export const sortAndCapitalizeLastName = (users: User[]) => {
-  const sortedUsers = [...users].sort((a, b) => a.name.localeCompare(b.name));
+  const sortedUsers = sortUsersByName(users);
+  const result = capitalizeLastnames(sortedUsers);
+  return result;
+};
 
-  const result = sortedUsers.map((user) => {
+export const sortUsersByName = (users: User[]) => {
+  return [...users].sort((a, b) => a.name.localeCompare(b.name));
+};
+
+export const capitalizeLastnames = (users: User[]) => {
+  return users.map((user) => {
     return {
       id: user.id,
       name: user.name,
       lastname: user.lastname.toUpperCase(),
-      email: user.email
+      email: user.email,
     };
   });
-  return result;
-}
+};
 
 export const filterUsersABC = (users: User[]) => {
-  const result = users.filter((user) => (
-    user.name[0].toLowerCase() === "a" || 
-    user.name[0].toLowerCase() === "b" || 
-    user.name[0].toLowerCase() === "c" 
-  ));
-  result.sort((a, b) => a.name.localeCompare(b.name));
-  return result;
-}
+  const result = users.filter(
+    (user) =>
+      user.name[0].toLowerCase() === "a" ||
+      user.name[0].toLowerCase() === "b" ||
+      user.name[0].toLowerCase() === "c"
+  );
+  return sortUsersByName(result);
+};
 
 export const countUsersABC = (users: User[]) => {
-  const aUsers = users.filter((user) => (user.name[0].toLowerCase() === "a"));
-  const bUsers = users.filter((user) => (user.name[0].toLowerCase() === "b"));
-  const cUsers = users.filter((user) => (user.name[0].toLowerCase() === "c"));
+  const aUsers = users.filter((user) => user.name[0].toLowerCase() === "a");
+  const bUsers = users.filter((user) => user.name[0].toLowerCase() === "b");
+  const cUsers = users.filter((user) => user.name[0].toLowerCase() === "c");
 
   const result = {
     a: aUsers.length,
@@ -35,4 +42,4 @@ export const countUsersABC = (users: User[]) => {
     c: cUsers.length,
   };
   return result;
-}
+};
